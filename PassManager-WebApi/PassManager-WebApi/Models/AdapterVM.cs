8667c50d9ac7ccfbbd0bd36396b9d7572976a4e1
HiMyNameIsGarch <gabrielhanu700@yahoo.com>
2020-10-11 19:22:19 +0300

    Add view models and adapters

---
+++ PassManager-WebApi/PassManager-WebApi/Models/AdapterVM.cs
@@ -0,0 +1,29 @@
+﻿using PassManager_WebApi.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PassManager_WebApi.Models
+{
+    public partial class Account
+    {
+        public Account()
+        {
+
+        }
+        public Account(AccountVM  account)
+        {
+            this.Title = account.Title;
+            this.Username = account.Username;
+            this.Email = account.Email;
+            this.Phone = account.Phone;
+            this.PasswordEncrypted = account.PasswordEncrypted;
+            this.SiteRegistered = account.SiteRegistered;
+            this.Notes = account.Notes;
+            this.CreateDate = DateTime.Today;
+            this.LastModified = DateTime.Now;
+            this.LastVisited = DateTime.Now;
+        }
+    }
+}
---
+++ PassManager-WebApi/PassManager-WebApi/ViewModels/AccountVM.cs
@@ -0,0 +1,28 @@
+﻿using PassManager_WebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PassManager_WebApi.ViewModels
+{
+    public class AccountVM : AccountPreviewVM
+    {
+        public AccountVM ()
+        {
+        }
+        public AccountVM(Account account) : base(account)
+        {
+            this.Username = account.Username;
+            this.Email = account.Email;
+            this.Phone = account.Phone;
+            this.PasswordEncrypted = account.PasswordEncrypted;
+            this.Notes = account.Notes;
+        }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string PasswordEncrypted { get; set; }
+        public string Notes { get; set; }
+    }
+}
