0ad7764cdadf92be8e2e1491fc5e8715cdfe9ce9
HiMyNameIsGarch <gabrielhnau700@yahoo.com>
2020-10-28 22:04:25 +0200

    add register and login action + secure storage

---
+++ PassManager-UI/PassManager-UI/PassManager-UI/Models/ResponseToken.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace PassManager.Models
+{
+    public class ResponseToken
+    {
+        public string access_token { get; set; }
+        public string token_type { get; set; }
+        public string error { get; set; }
+        public string error_description { get; set; }
+    }
+}
