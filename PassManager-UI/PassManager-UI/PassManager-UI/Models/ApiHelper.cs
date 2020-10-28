8ca742ffb3d02a5220fdf34fea09055263882699
HiMyNameIsGarch <gabrielhnau700@yahoo.com>
2020-10-28 18:15:27 +0200

    add api helper

---
+++ PassManager-UI/PassManager-UI/PassManager-UI/Models/ApiHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace PassManager.Models
+{
+    public class ApiHelper
+    {
+        internal const string SERVER = "https://localhost:44364";
+        private static HttpClient _httpClient;
+        internal static HttpClient ApiClient
+        {
+            get
+            {
+                _httpClient = _httpClient ?? new HttpClient
+                (
+                    new HttpClientHandler()
+                    {
+                        ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) =>
+                        {
+                            //bypass
+                            return true;
+                        },
+                    }
+                    , false
+                );
+                //_httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Authorization", "TOKEN");
+                return _httpClient;
+            }
+        }
+        internal static void InitializeClient()
+        {
+            ApiClient.BaseAddress = new Uri(SERVER);
+            ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
