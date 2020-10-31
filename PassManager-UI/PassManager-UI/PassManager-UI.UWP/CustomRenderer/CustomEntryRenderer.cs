5a1540952d42ee5d1912bf02afc1754176aecfbc
HiMyNameIsGarch <gabrielhnau700@yahoo.com>
2020-10-31 17:54:50 +0200

    add custom entry renderer for android and uwp

---
+++ PassManager-UI/PassManager-UI/PassManager-UI.UWP/CustomRenderer/CustomEntryRenderer.cs
@@ -0,0 +1,32 @@
+﻿using PassManager.CustomRenderer;
+using PassManager_UI.UWP.CustomRenderer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Media;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.UWP;
+
+[assembly: ExportRenderer(typeof(CustomeEntry), typeof(CustomEntryRenderer))]
+namespace PassManager_UI.UWP.CustomRenderer
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if(Control != null)
+            {
+                var transparentBrush = new Windows.UI.Xaml.Media.SolidColorBrush(Colors.Transparent);
+                Control.Background = transparentBrush;
+                Control.BorderBrush = transparentBrush;
+                Control.Margin = new Windows.UI.Xaml.Thickness(15,5,15,5);
+            }
+        }
+    }
+}
