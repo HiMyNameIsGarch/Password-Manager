6baa9619978ae1ea677d0227bee05237b5a25b47
HiMyNameIsGarch <gabrielhnau700@yahoo.com>
2020-11-01 16:48:55 +0200

    change visibility of password field from image btn

---
+++ PassManager-UI/PassManager-UI/PassManager-UI/MarkupExtensions/EmbeddedImage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PassManager.MarkupExtensions
+{
+    [ContentProperty("ResourceId")]
+    public class EmbeddedImage : IMarkupExtension
+    {
+        public string ResourceId { get; set; }
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (string.IsNullOrWhiteSpace(ResourceId)) return null;
+            return ImageSource.FromResource(ResourceId);
+        }
+    }
+}
