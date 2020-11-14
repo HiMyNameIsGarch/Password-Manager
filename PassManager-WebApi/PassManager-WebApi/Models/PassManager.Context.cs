cb522e9bacc8a6d73e73d0658fdac1ed8068c689
HiMyNameIsGarch <gabrielhanu700@yahoo.com>
2020-11-14 23:39:40 +0200

    change tables from database

--- PassManager-WebApi/PassManager-WebApi/Models/PassManager.Context.cs
+++ PassManager-WebApi/PassManager-WebApi/Models/PassManager.Context.cs
@@ -25,11 +25,12 @@ namespace PassManager_WebApi.Models
             throw new UnintentionalCodeFirstException();
         }
     
-        public virtual DbSet<Account> Accounts { get; set; }
         public virtual DbSet<C__MigrationHistory> C__MigrationHistory { get; set; }
         public virtual DbSet<AspNetRole> AspNetRoles { get; set; }
         public virtual DbSet<AspNetUserClaim> AspNetUserClaims { get; set; }
         public virtual DbSet<AspNetUserLogin> AspNetUserLogins { get; set; }
         public virtual DbSet<AspNetUser> AspNetUsers { get; set; }
+        public virtual DbSet<Password> Passwords { get; set; }
+        public virtual DbSet<Wifi> Wifis { get; set; }
     }
 }
---
+++ PassManager-WebApi/PassManager-WebApi/ViewModels/WifiVM.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PassManager_WebApi.ViewModels
+{
+    public class WifiVM
+    {
+        public WifiVM() { }
+        public WifiVM(Models.Wifi wifi)
+        {
+            Id = wifi.Id;
+            Name = wifi.Name;
+            PasswordEncrypted = wifi.PasswordEncrypted;
+            Notes = wifi.Notes;
+            CreateDate = DateTime.Now;
+            LastVisited = DateTime.Now;
+            LastModified = DateTime.Now;
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PasswordEncrypted { get; set; }
+        public string Notes { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime LastVisited { get; set; }
+        public DateTime LastModified { get; set; }
+    }
+}
