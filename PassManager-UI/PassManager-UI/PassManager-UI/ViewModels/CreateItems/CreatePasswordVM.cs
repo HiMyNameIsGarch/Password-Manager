deeb3c37bdfc0e27f2f6284bdcd2aafe045f6355
HiMyNameIsGarch <gabrielhanu700@yahoo.com>
2020-12-14 23:40:28 +0200

    add more options for user when on item

--- PassManager-UI/PassManager-UI/PassManager-UI/ViewModels/CreateItems/CreatePasswordVM.cs
+++ PassManager-UI/PassManager-UI/PassManager-UI/ViewModels/CreateItems/CreatePasswordVM.cs
@@ -11,75 +11,19 @@ using PassManager.Models;
 namespace PassManager.ViewModels.CreateItems
 {
     //parameters
-    [QueryProperty("PageType", "pageType")]
-    [QueryProperty("Id","id")]
     public class CreatePasswordVM : BaseItemVM, IBackButtonBehavior
     {
         public CreatePasswordVM()
         {
-            //set defaults values in case no parameter passed
-            ChangeProps(ItemPageState.Null,"Create","No data provided",true);
             _goBack = new Command(GoBackButton);
         }
         //variables
-        private string _id;
         private Password _password = new Password();
-        private string _pageType;
         private ICommand _goBack;
         //props
-        public string Id
-        {
-            get { return _id; }
-            set 
-            {
-                _id = Uri.UnescapeDataString(value ?? string.Empty);
-                if(PageState != ItemPageState.Null)
-                {
-                    if(int.TryParse(_id, out int newId))
-                    {
-                        GetData(newId).Await();
-                    }
-                    else
-                    {
-                        //handle error from id
-                    }
-                }
-                else
-                {
-                    //handle error
-                    PageTitle = "Your item is invalid!";
-                }
-            }
-        }
-        public string PageType
-        {
-            get { return _pageType; }
-            set 
-            {
-                _pageType = Uri.UnescapeDataString(value ?? string.Empty);
-                Enum.TryParse(_pageType, out ItemPageState pageState);
-                PageState = pageState;
-                switch (PageState)
-                {
-                    case ItemPageState.Create:
-                        PageTitle = "Create Password!";
-                        ReadOnly = false;
-                        break;
-                    case ItemPageState.View:
-                        ChangeProps(ItemPageState.View, "Edit", "View Password", true);
-                        break;
-                    case ItemPageState.Edit:
-                        PageTitle = "Edit Password";
-                        break;
-                    default:
-                        PageTitle = "Your item is invalid!";
-                        break;
-                }
-            }
-        }
         public Password Password {
             get { return _password; }
-            set { _password = value; } 
+            set { _password = value; NotifyPropertyChanged(); } 
         }
         //commands
         public ICommand GoBack
@@ -105,16 +49,15 @@ namespace PassManager.ViewModels.CreateItems
             if(password != null)
             {
                 Password = password;
-                NotifyPropertyChanged("Password");
             }
         }
         //override basic actions for password
         private protected async override Task Create()
         {
             bool isSuccess = await PasswordProcessor.CreatePassword(ApiHelper.ApiClient,Password);
-            if (true)
+            if (isSuccess)
             {
-                await GoTo("Password");
+                await GoTo("Password", "?update=true");
             }
             else
             {
@@ -149,6 +92,45 @@ namespace PassManager.ViewModels.CreateItems
             else
             {
                 //handle errors
+            }
+        }
+
+        private protected override void AfterSettingId()
+        {
+            switch (PageState)
+            {
+                case ItemPageState.Create:
+                    PageTitle = "Create Password!";
+                    ReadOnly = false;
+                    break;
+                case ItemPageState.View:
+                    ChangeProps(ItemPageState.View, "Edit", "View Password", true);
+                    break;
+                case ItemPageState.Edit:
+                    PageTitle = "Edit Password";
+                    break;
+                default:
+                    PageTitle = "Your item is invalid!";
+                    break;
+            }
+        }
+        private protected override void AfterSettingPageType()
+        {
+            if (PageState != ItemPageState.Null)
+            {
+                if (int.TryParse(Id, out int newId))
+                {
+                    GetData(newId).Await();
+                }
+                else
+                {
+                    //handle error from id
+                }
+            }
+            else
+            {
+                //handle error
+                PageTitle = "Your item is invalid!";
             }
         }
     }
