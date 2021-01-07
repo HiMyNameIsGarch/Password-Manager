ed7969505d864c0c209569fb7c08da94903a35bf
HiMyNameIsGarch <gabrielhanu700@yahoo.com>
2021-01-08 01:00:47 +0200

    add a search page

---
+++ PassManager-UI/PassManager-UI/PassManager-UI/Views/SearchView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using PassManager.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PassManager.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SearchView : ContentPage
+    {
+        public SearchView()
+        {
+            InitializeComponent();
+            BindingContext = new SearchViewModel();
+        }
+    }
+}
