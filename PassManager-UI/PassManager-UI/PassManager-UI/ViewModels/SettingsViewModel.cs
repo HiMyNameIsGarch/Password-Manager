685f0aa120c2eb92048ef692f4f9a14810191ad7
HiMyNameIsGarch <gabrielhnau700@yahoo.com>
2021-01-14 20:57:13 +0200

    allow user to sign out from account when user runs out of items, display a message change how you add an item

---
+++ PassManager-UI/PassManager-UI/PassManager-UI/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,42 @@
+﻿using PassManager.Models;
+using PassManager.Models.Api;
+using System.Collections.Generic;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace PassManager.ViewModels
+{
+    public class SettingsViewModel : BaseViewModel
+    {
+        public SettingsViewModel() : base("Settings")
+        {
+        }
+        public IEnumerable<SettingModel> AllSettings { 
+            get
+            {
+                return new List<SettingModel>()
+                {
+                    new SettingModel("Delete Account", new Command( () => 
+                    {
+
+                    })),
+                    new SettingModel("Sign Out", new Command( () => 
+                    {
+                        ApiHelper.DeleteAuthorization();
+                        PageService.ChangeMainPage(new Views.AuthenticationView());
+                    })),
+                };
+            }
+        }
+    }
+    public class SettingModel
+    {
+        public SettingModel(string text, ICommand command)
+        {
+            Text = text;
+            WhenClicked = command;
+        }
+        public string Text { get; set; }
+        public ICommand WhenClicked { get; set; }
+    }
+}
