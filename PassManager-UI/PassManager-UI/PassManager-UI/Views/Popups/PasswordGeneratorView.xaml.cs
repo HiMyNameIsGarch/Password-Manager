f73076b6084056599fd0654b49416d27b0810c29
HiMyNameIsGarch <gabrielhnau700@yahoo.com>
2021-01-16 16:24:53 +0200

    add a simple password generator

---
+++ PassManager-UI/PassManager-UI/PassManager-UI/Views/Popups/PasswordGeneratorView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Rg.Plugins.Popup.Pages;
+using PassManager.ViewModels.Popups;
+using Xamarin.Forms.Xaml;
+
+namespace PassManager.Views.Popups
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PasswordGeneratorView : PopupPage
+    {
+        public PasswordGeneratorView()
+        {
+            InitializeComponent();
+            BindingContext = new PasswordGeneratorVM();
+        }
+    }
+}
