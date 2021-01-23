ebcb4d2fc4e8678188ba5d05d8b66ea093226d07
HiMyNameIsGarch <gabrielhnau700@yahoo.com>
2021-01-23 18:42:36 +0200

    add encrypt/decrypt methods for vaultmanager

--- PassManager-UI/PassManager-UI/PassManager-UI/Models/VaultManager.cs
+++ PassManager-UI/PassManager-UI/PassManager-UI/Models/VaultManager.cs
@@ -15,5 +15,61 @@ namespace PassManager.Models
             authPass = Convert.ToBase64String(SecurePassword.HashPassword(email, authPass, 5000));//then hash it 5000 times on client side
             return authPass;//return it
         }
+        public static string EncryptString(string plainText)
+        {
+            if (string.IsNullOrEmpty(plainText)) return string.Empty;
+
+            byte[] array;
+            using (Aes aes = CreateCipher())
+            {
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter streamWriter = new StreamWriter((Stream)cryptoStream))
+                        {
+                            streamWriter.Write(plainText);
+                        }
+                        array = memoryStream.ToArray();
+                    }
+                }
+            }
+            return Convert.ToBase64String(array);
+        }
+        public static string DecryptString(string textToDecrypt)
+        {
+            if (string.IsNullOrEmpty(textToDecrypt)) return string.Empty;
+
+            byte[] buffer = Convert.FromBase64String(textToDecrypt);
+
+            using (Aes aes = CreateCipher())
+            {
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                using (MemoryStream memoryStream = new MemoryStream(buffer))
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader streamReader = new StreamReader((Stream)cryptoStream))
+                        {
+                            return streamReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+        }
+        private static Aes CreateCipher()
+        {
+            var cipher = Aes.Create();
+
+            cipher.Padding = PaddingMode.ISO10126;
+
+            cipher.Key = VaultKey;
+
+            cipher.IV = new byte[16];
+
+            return cipher;
+        }
     }
 }
