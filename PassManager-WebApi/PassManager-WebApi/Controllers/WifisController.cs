53cb6be3404eddcdc16b26669967a88237994d5b
HiMyNameIsGarch <gabrielhanu700@yahoo.com>
2021-01-25 18:21:14 +0200

    modify wifi item and add a controller for it

---
+++ PassManager-WebApi/PassManager-WebApi/Controllers/WifisController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNet.Identity;
+using PassManager_WebApi.Enums;
+using PassManager_WebApi.Models;
+using PassManager_WebApi.ViewModels;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace PassManager_WebApi.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/Wifis")]
+    public class WifisController : ApiController
+    {
+        private PasswordManagerEntities db = new PasswordManagerEntities();
+
+        //GET api/Wifis
+        public IHttpActionResult Get()
+        {
+            string userId = User.Identity.GetUserId();
+            //take all ordered wifis
+            IEnumerable<ItemPreview> wifis = db.Wifis
+                .Where(item => item.UserId == userId)
+                .OrderByDescending(item => item.NumOfVisits)
+                .ThenByDescending(item => item.Name)
+                .Select(item => new ItemPreview() { Id = item.Id, Title = item.Name, SubTitle = "Wi-Fi", ItemType = TypeOfItems.Wifi })
+                .ToList();
+            //group them
+            IEnumerable<Grouping<TypeOfItems, ItemPreview>> groupedWifis = wifis
+                .GroupBy(item => item.ItemType)
+                .Select(item => new Grouping<TypeOfItems, ItemPreview>(item.Key, item));
+            return Ok(groupedWifis);
+        }
+        //GET api/Wifis/5
+        public IHttpActionResult Get(int id)
+        {
+            if (id <= 0) return BadRequest("Id is invalid");
+            string userId = User.Identity.GetUserId();
+            //get current wifi based on userId
+            Wifi wifi = db.Wifis.FirstOrDefault(w => w.Id == id && w.UserId == userId);
+            if (wifi is null) return BadRequest("Wifi does not exist!");
+            wifi.NumOfVisits++;
+            db.SaveChanges();
+            return Ok(new WifiVM(wifi));
+        }
+        //POST api/Wifi
+        public IHttpActionResult Post([FromBody] WifiVM wifi)
+        {
+            if (wifi is null) return BadRequest("Wifi does not exist!");
+            var isModelValid = wifi.IsModelValid();
+            if (!string.IsNullOrEmpty(isModelValid)) return BadRequest(isModelValid);
+            string userId = User.Identity.GetUserId();
+            db.Wifis.Add(new Wifi(wifi, userId));
+            db.SaveChanges();
+            return Ok();
+        }
+        //PUT api/Wifi/5
+        public IHttpActionResult Put(int id, [FromBody] WifiVM wifi)
+        {
+            //check if the wifi and the id match
+            if (wifi is null) return BadRequest("Wifi does not exist!");
+            if (id != wifi.Id) return BadRequest("Id does not match with the wifi");
+            if (id <= 0) return BadRequest("Id is invalid");
+            //check if wifi is valid
+            var isModelValid = wifi.IsModelValid();
+            if (!string.IsNullOrEmpty(isModelValid)) return BadRequest(isModelValid);
+            //get current user id and modify wifi
+            string userId = User.Identity.GetUserId();
+            Wifi wifiToBeModified = db.Wifis.FirstOrDefault(w => w.Id == id && w.UserId == userId);
+            if (wifiToBeModified is null) return BadRequest("Wifi not found");
+            wifiToBeModified.ModifyTo(wifi);
+            db.SaveChanges();
+            return Ok();
+        }
+        //DELETE api/Wifi/5
+        public IHttpActionResult Delete(int id)
+        {
+            if (id <= 0) return BadRequest("Id is invalid");
+            string userId = User.Identity.GetUserId();
+            Wifi wifiToBeDeleted = db.Wifis.FirstOrDefault(w => w.Id == id && w.UserId == userId);
+            if (wifiToBeDeleted is null) return BadRequest("Wifi not found");
+            db.Wifis.Remove(wifiToBeDeleted);
+            db.SaveChanges();
+            return Ok();
+        }
+    }
+}
--- PassManager-WebApi/PassManager-WebApi/ViewModels/WifiVM.cs
+++ PassManager-WebApi/PassManager-WebApi/ViewModels/WifiVM.cs
@@ -11,15 +11,30 @@ namespace PassManager_WebApi.ViewModels
             Id = wifi.Id;
             Name = wifi.Name;
             PasswordEncrypted = wifi.PasswordEncrypted;
+            SSID = wifi.SSID;
+            SettingsPassword = wifi.SettingsPassword;
+            ConnectionType = wifi.ConnectionType;
             Notes = wifi.Notes;
         }
         public int Id { get; set; }
         public string Name { get; set; }
         public string PasswordEncrypted { get; set; }
         public string Notes { get; set; }
+        public string SSID { get; set; }
+        public string SettingsPassword { get; set; }
+        public string ConnectionType { get; set; }
+
         public string IsModelValid()
         {
-            throw new System.NotImplementedException();
+            if(string.IsNullOrEmpty(Name) || string.IsNullOrEmpty(PasswordEncrypted))
+                return "You need to complete at least \"Name\" and \"Password\" in order to save!";
+            if (Name.Length > 64)
+                return "Your Name must be maximum 64 characters!";
+            if (ConnectionType?.Length > 64)
+                return "Your Connection Type must be maximum 64 characters!";
+            if (SSID?.Length > 64)
+                return " must be maximum 64 characters!";
+            return string.Empty;
         }
     }
 }
